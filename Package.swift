21cb7097a1623014085803befb6acf0e1d389a73
Antoine van der Lee <ajvanderlee@gmail.com>
2020-11-27 16:27:20 +0100

    Add SwiftPM support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Trekker",
+    platforms: [
+        // Only add support for iOS 12 and up.
+        .iOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Trekker",
+            targets: ["Trekker"]),
+    ],
+    targets: [
+        .target(
+            name: "Trekker",
+            dependencies: [])
+    ]
+)
